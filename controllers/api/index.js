eb8832748b43baf984b990c376fe9f8d0df91cca
MarkBrooks1985 <alynnnari@gmail.com>
2022-09-22 13:25:59 +0930

    9th commit, an update to routes and misc content

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,11 @@
-// index.js needs doing
+const router = require("express").Router();
+
+const blogRoutes = require("./blogRoutes");
+const commentRoutes = require("./commentRoutes");
+const userRoutes = require("./userRoutes");
+
+router.use("/", blogRoutes);
+router.use("/", commentRoutes);
+router.use("/", userRoutes);
+
+module.exports = router;
