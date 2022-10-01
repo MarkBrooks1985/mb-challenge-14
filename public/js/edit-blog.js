27cf59ff65e9b0c7edca9911cfa53a0820ad8089
MarkBrooks1985 <alynnnari@gmail.com>
2022-10-01 18:48:43 +0930

    18th commit, another update

--- public/js/edit-blog.js
+++ public/js/edit-blog.js
@@ -4,24 +4,21 @@ const editBlog = async (event) => {
   const id = document.querySelector("#disabled-text").value.trim();
   const title = document.querySelector("#title-entry").value.trim();
   const content = document.querySelector("#content-entry").value.trim();
-  const username = document.querySelector("#username-entry").value.trim();
 
-  if (id && title && content && username) {
-    const response = await fetch(`/api/user/${username}`);
-
+  if (id && title && content) {
     if (response.ok) {
       const userData = await response.json();
 
       const user_id = userData.id;
 
-      const putResponse = await fetch(`/api/blogpost/${user_id}`, {
+      const putResponse = await fetch(`/api/blog/${blog_id}`, {
         method: "PUT",
-        body: JSON.stringify({ id, title, content, user_id }),
+        body: JSON.stringify({ id, title, content, blog_id }),
         headers: { "Content-Type": "application/json" },
       });
 
       if (putResponse.ok) {
-        document.location.replace(`/post/${user_id}`);
+        document.location.replace(`/blog/${blog_id}`);
       }
     }
   } else {
@@ -29,6 +26,4 @@ const editBlog = async (event) => {
   }
 };
 
-document
-  .querySelector("#editPostBtn")
-  .addEventListener("click", updateBlogPost);
+document.querySelector("#editBlogBtn").addEventListener("click", editBlog);
